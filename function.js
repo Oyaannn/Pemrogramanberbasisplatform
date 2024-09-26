b91a8e44cb747a23b85064d71019401cfcd174cf
Irpan Arroyan <126502869+Oyaannn@users.noreply.github.com>
2024-09-26 13:21:17 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,16 @@
+//traditional fuction
+
+function add2number(num1,num2){
+    let result = num1 + num2
+    return result
+}
+
+//Arrow Fuction
+
+let divide2Number = (num1,num2) => {
+    let result = num1 / num2
+    return result
+}
+
+console.log(add2number(10,50));
+console.log(divide2Number(50,10));  
---
+++ hello.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+
+var firstname = "Agus";
+var lastname = "Kasep";
+
+//my name is agus kasep
+console.log("my name is" + " " + firstname +" "+ lastname); 
+
+//string literal
+console.log(`my name is ${firstname}${lastname}`)
+
+var num1 = 50;
+var num2 = 100;
+
+console.log(`multiply result is ${num1 * num2}`)
